2d2c3c008a59e514037af2b51665a4eeb19da056
Rogerin <rogeric3081@gmail.com>
2025-01-07 21:35:13 -0400

    Lab 1

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Diplomado FullStack</h1>
+      <h3>Modulo 7 React</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
