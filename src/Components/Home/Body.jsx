4abbab50dd0f2b458ec6c6d08bb0710c8fe0a90c
Rogerin <rogeric3081@gmail.com>
2025-01-08 20:51:39 -0400

    creando componentes en home

---
+++ src/Components/Home/Body.jsx
@@ -0,0 +1,14 @@
+const Body = () =>{
+    return(
+        <div className="card">
+            <h1>Vite + React</h1>
+            
+            <h2>Diplomado en Fullstack</h2>
+            <h3>Modulo 7 React</h3>
+            <p>
+                Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+        </div>
+    );
+};
+export default Body;
