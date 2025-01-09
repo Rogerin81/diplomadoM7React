35852a5ed64385b7684f646f7297ee2a2ed629c1
Rogerin <rogeric3081@gmail.com>
2025-01-08 21:57:28 -0400

    actualizando

--- src/Components/Core/OpenLink.jsx
+++ src/Components/Core/OpenLink.jsx
@@ -1,10 +1,10 @@
 const OpenLink =({ redirectURL, logoImage,  altDescription}) =>{
     return(
-        <div>
+        <>
              <a href={redirectURL} target="_blank">
                 <img src={logoImage} className="logo" alt={`${altDescription} logo`}/>
             </a>
-        </div>
+        </>
     );
 };
 export default OpenLink;
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -10,19 +10,11 @@ import GitHubLogo from "../assets/github.svg";
 const Home = () =>{
     return (
         <div>
-            <table>
-                <tr>
-                    <td>
-                        <OpenLink redirectURL="https://vite.dev" logoImage={viteLogo} altDescription="Vite"/>
-                    </td>
-                    <td>
-                        <OpenLink redirectURL="https://react.dev" logoImage={reactLogo} altDescription="React"/>
-                    </td>
-                    <td>
-                        <OpenLink redirectURL="https://github.com" logoImage={GitHubLogo} altDescription="Github"/>
-                    </td>
-                </tr>
-            </table>
+            <div>
+            <OpenLink redirectURL="https://vite.dev" logoImage={viteLogo} altDescription="Vite"/>
+            <OpenLink redirectURL="https://react.dev" logoImage={reactLogo} altDescription="React"/>
+            <OpenLink redirectURL="https://github.com" logoImage={GitHubLogo} altDescription="Github"/>
+            </div>
             <Body />
             <Footer />
         </div>
