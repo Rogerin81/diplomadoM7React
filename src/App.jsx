ea74c622c9a6fe538dae09b9a3001e5e3668c6f9
Rogerin <rogeric3081@gmail.com>
2025-01-09 21:19:49 -0400

    día3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Counter from './Components/Counter/Counter';
 
 import Home from './Screens/Home';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Home />
+      <Counter />
     </>
   )
 }
