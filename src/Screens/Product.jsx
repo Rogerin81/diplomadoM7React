e3f5970a48591bdfbc8e9423a61bd1167815aead
Rogerin <rogeric3081@gmail.com>
2025-01-14 23:11:19 -0400

    martes14Tarea1

--- src/Screens/Product.jsx
+++ src/Screens/Product.jsx
@@ -1,15 +1,7 @@
-import FilterProductTable from "../Components/Product/FilterableProductTable.jsx";
+import FilterProductTable from "../Components/Product/FilterableProductTable";
 const Product = () => {
     return (
         <div>
-            <div>
-
-            <h1> Practica N1 REACT</h1>
-            <h2> Producto, Thinking in React</h2>
-            <h3> Realizado: por Luis Carlos Iturricha</h3>
-            <h3> Docente: Ing. William Roddy Barra Paredes</h3>
-            </div>
-            
             <FilterProductTable/>
             
         </div>
